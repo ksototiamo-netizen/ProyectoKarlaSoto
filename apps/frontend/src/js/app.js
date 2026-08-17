08b7306b1aa1115343c555222f53e08e6437c90b
kei <ksototiamo@gmail.com>
2026-08-17 18:36:55 -0400

    arreglos

---
+++ apps/frontend/src/js/app.js
@@ -0,0 +1,5 @@
+const characterGrid = document.getElementById('character-grid');
+const filterButtons = document.querySelectorAll('.elemental-filters button');
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+
